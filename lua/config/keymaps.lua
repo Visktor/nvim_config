906609c3e856ed909d831a64236a8da2d0b0e845
visktor <webdev.vitor@gmail.com>
2024-08-15 17:48:35 -0300

    .

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,15 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set(
+  { "n", "v", "i", "t" },
+  "<F4>",
+  "<cmd>ToggleTerm direction=float<cr>",
+  { desc = "Toggle Terminal Float" }
+)
+
+vim.keymap.set({ "n" }, "<leader>fdw", "<cmd>Telescope dir live_grep<cr>", { desc = "Search Word in Directory" })
+vim.keymap.set({ "n" }, "<leader>fdf", "<cmd>Telescope dir live_grep<cr>", { desc = "Search File in Directory" })
+vim.keymap.set({ "n" }, "<leader>n", "<cmd>nohlsearch<cr>", { desc = "Disable Highlighted Search" })
+vim.keymap.set({ "n" }, "s", "<cmd>HopWord<cr>", { desc = "Disable Highlighted Search" })
