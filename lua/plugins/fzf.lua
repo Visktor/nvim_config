194f3204fa2371a27192d638d3976ec6e7eab5b4
Vitor Barros <vitor-barros@DEV-RECIFE-0629.local>
2024-09-22 20:27:36 -0300

    didnt work fuccc

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -2,12 +2,13 @@ return {
   "ibhagwan/fzf-lua",
   cmd = "FzfLua",
   opts = function(_, opts)
-    return vim.tbl_deep_extend("force", opts, {
-      files = vim.tbl_deep_extend("force", opts.files, {
-        actions = vim.tbl_deep_extend("force", opts.files.actions, {
-          ["default"] = require("fzf-lua.actions").file_edit,
-        }),
-      }),
-    })
+    return opts
+    -- return vim.tbl_deep_extend("force", opts, {
+    -- files = vim.tbl_deep_extend("force", opts.files, {
+    -- actions = vim.tbl_deep_extend("force", opts.files.actions, {
+    --   ["default"] = require("fzf-lua.actions").file_edit,
+    -- }),
+    -- }),
+    -- })
   end,
 }
