f7a8498234c007a6c71c85ac8e730e7f7a60ba08
Vitor Barros <vitor-barros@DEV-RECIFE-0629.local>
2024-10-27 17:16:36 -0300

    cmp comparator

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,5 +1,56 @@
 return {
   {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local types = require("cmp.types")
+      local compare = require("cmp.config.compare")
+
+      ---@type table<integer, integer>
+      local modified_priority = {
+        [types.lsp.CompletionItemKind.Variable] = types.lsp.CompletionItemKind.Method,
+        [types.lsp.CompletionItemKind.Snippet] = 0, -- top
+        [types.lsp.CompletionItemKind.Keyword] = 0, -- top
+        [types.lsp.CompletionItemKind.Text] = 100, -- bottom
+      }
+      ---@param kind integer: kind of completion entry
+      local function modified_kind(kind)
+        return modified_priority[kind] or kind
+      end
+      opts.sorting = vim.tbl_extend("force", opts.sorting, {
+        comparators = {
+          compare.offset,
+          compare.exact,
+          compare.score,
+          compare.order,
+          function(entry1, entry2) -- sort by length ignoring "=~"
+            local len1 = string.len(string.gsub(entry1.completion_item.label, "[=~()_]", ""))
+            local len2 = string.len(string.gsub(entry2.completion_item.label, "[=~()_]", ""))
+            if len1 ~= len2 then
+              return len1 - len2 < 0
+            end
+          end,
+          compare.recently_used,
+          function(entry1, entry2) -- sort by compare kind (Variable, Function etc)
+            local kind1 = modified_kind(entry1:get_kind())
+            local kind2 = modified_kind(entry2:get_kind())
+            if kind1 ~= kind2 then
+              return kind1 - kind2 < 0
+            end
+          end,
+          function(entry1, entry2) -- score by lsp, if available
+            local t1 = entry1.completion_item.sortText
+            local t2 = entry2.completion_item.sortText
+            if t1 ~= nil and t2 ~= nil and t1 ~= t2 then
+              return t1 < t2
+            end
+          end,
+        },
+      })
+      return opts
+    end,
+  },
+  {
     "nvim-treesitter/nvim-treesitter",
     opts = function(_, opts)
       -- add tsx and treesitter
@@ -7,7 +58,6 @@ return {
         "tsx",
         "typescript",
       })
-
     end,
   },
   {
