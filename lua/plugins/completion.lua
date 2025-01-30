d476bb18951caee0dc014e610900e58f90cd1346
Vitor Barros <vitor-barros@DEV-RECIFE-0629.local>
2025-01-29 22:31:05 -0300

    seinao

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,133 @@
+return {
+  {
+    "xzbdmw/colorful-menu.nvim",
+    config = function()
+      -- You don't need to set these options.
+      require("colorful-menu").setup({
+        ls = {
+          lua_ls = {
+            -- Maybe you want to dim arguments a bit.
+            arguments_hl = "@comment",
+          },
+          gopls = {
+            -- By default, we render variable/function's type in the right most side,
+            -- to make them not to crowd together with the original label.
+
+            -- when true:
+            -- foo             *Foo
+            -- ast         "go/ast"
+
+            -- when false:
+            -- foo *Foo
+            -- ast "go/ast"
+            align_type_to_right = true,
+            -- When true, label for field and variable will format like "foo: Foo"
+            -- instead of go's original syntax "foo Foo".
+            add_colon_before_type = false,
+          },
+          -- for lsp_config or typescript-tools
+          ts_ls = {
+            extra_info_hl = "@comment",
+          },
+          vtsls = {
+            extra_info_hl = "@comment",
+          },
+          ["rust-analyzer"] = {
+            -- Such as (as Iterator), (use std::io).
+            extra_info_hl = "@comment",
+          },
+          clangd = {
+            -- Such as "From <stdio.h>".
+            extra_info_hl = "@comment",
+          },
+          roslyn = {
+            extra_info_hl = "@comment",
+          },
+          -- The same applies to pyright/pylance
+          basedpyright = {
+            -- It is usually import path such as "os"
+            extra_info_hl = "@comment",
+          },
+
+          -- If true, try to highlight "not supported" languages.
+          fallback = true,
+        },
+        -- If the built-in logic fails to find a suitable highlight group,
+        -- this highlight is applied to the label.
+        fallback_highlight = "@variable",
+        -- If provided, the plugin truncates the final displayed text to
+        -- this width (measured in display cells). Any highlights that extend
+        -- beyond the truncation point are ignored. When set to a float
+        -- between 0 and 1, it'll be treated as percentage of the width of
+        -- the window: math.floor(max_width * vim.api.nvim_win_get_width(0))
+        -- Default 60.
+        max_width = 60,
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "onsails/lspkind.nvim" },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local types = require("cmp.types")
+      local compare = require("cmp.config.compare")
+
+      local lspkind = require("lspkind")
+
+      opts.formatting = vim.tbl_deep_extend("force", opts.formatting, {
+        format = lspkind.cmp_format({
+          mode = "symbol", -- show only symbol annotations
+          maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+        }),
+      })
+
+      ---@type table<integer, integer>
+      local modified_priority = {
+        [types.lsp.CompletionItemKind.Variable] = types.lsp.CompletionItemKind.Method,
+        [types.lsp.CompletionItemKind.Snippet] = 0, -- top
+        [types.lsp.CompletionItemKind.Keyword] = 0, -- top
+        [types.lsp.CompletionItemKind.Text] = 100, -- bottom
+      }
+      ---@param kind integer: kind of completion entry
+      local function modified_kind(kind)
+        return modified_priority[kind] or kind
+      end
+      opts.sorting = vim.tbl_extend("force", opts.sorting, {
+        comparators = {
+          compare.offset,
+          compare.exact,
+          compare.score,
+          compare.order,
+          function(entry1, entry2) -- sort by length ignoring "=~"
+            local len1 = string.len(string.gsub(entry1.completion_item.label, "[=~()_]", ""))
+            local len2 = string.len(string.gsub(entry2.completion_item.label, "[=~()_]", ""))
+            if len1 ~= len2 then
+              return len1 - len2 < 0
+            end
+          end,
+          compare.recently_used,
+          function(entry1, entry2) -- sort by compare kind (Variable, Function etc)
+            local kind1 = modified_kind(entry1:get_kind())
+            local kind2 = modified_kind(entry2:get_kind())
+            if kind1 ~= kind2 then
+              return kind1 - kind2 < 0
+            end
+          end,
+          function(entry1, entry2) -- score by lsp, if available
+            local t1 = entry1.completion_item.sortText
+            local t2 = entry2.completion_item.sortText
+            if t1 ~= nil and t2 ~= nil and t1 ~= t2 then
+              return t1 < t2
+            end
+          end,
+        },
+      })
+      return opts
+    end,
+  },
+}
