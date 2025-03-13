e2f8683c81d9836787b27011976bb30cddad2a63
Vitor Barros <webdev.vitor@gmail.com>
2025-03-13 13:05:27 -0300

    ai dento

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,7 +17,7 @@ return {
             text = ("[%d]"):format(idx)
             chunks = { { " ", "Ignore" }, { text, "HlSearchLens" } }
           end
-          render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
+         render.setVirt(0, lnum - 1, col - 1, chunks, nearest)
         end
         local lensBak
         local config = require("hlslens.config")
@@ -156,21 +156,21 @@ return {
     event = { "BufReadPre", "BufNewFile" },
     config = true,
   },
-  -- {
-  --   "f-person/auto-dark-mode.nvim",
-  --   config = {
-  --     update_interval = 1000,
-  --     set_dark_mode = function()
-  --       require("notify")("Dark Mode")
-  --       vim.cmd("colorscheme tokyonight-storm")
-  --     end,
-  --     set_light_mode = function()
-  --       require("notify")("Light Mode")
-  --       vim.cmd("set background=light")
-  --       vim.cmd("colorscheme catppuccin-latte")
-  --     end,
-  --   },
-  -- },
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = {
+      update_interval = 100000,
+      set_dark_mode = function()
+        require("notify")("Dark Mode")
+        vim.cmd("colorscheme tokyonight-storm")
+      end,
+      set_light_mode = function()
+        require("notify")("Light Mode")
+        vim.cmd("set background=light")
+        vim.cmd("colorscheme catppuccin-latte")
+      end,
+    },
+  },
   {
     "nacro90/numb.nvim",
     config = function()
