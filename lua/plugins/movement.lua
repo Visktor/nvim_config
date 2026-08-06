6e1d01d15638e8772cb1fcfa7c40f6c8b7277630
Vitor Barros <webdev.vitor@gmail.com>
2026-08-06 09:17:30 -0300

    fix(movement): disable treewalker jump highlight

--- lua/plugins/movement.lua
+++ lua/plugins/movement.lua
@@ -122,14 +122,7 @@ return {
     -- and setup() does not need to be called, so the whole opts block is optional as well.
     opts = {
       -- Whether to briefly highlight the node after jumping to it
-      highlight = true,
-
-      -- How long should above highlight last (in ms)
-      highlight_duration = 250,
-
-      -- The color of the above highlight. Must be a valid vim highlight group.
-      -- (see :h highlight-group for options)
-      highlight_group = "CursorLine",
+      highlight = false,
     },
   },
 }
