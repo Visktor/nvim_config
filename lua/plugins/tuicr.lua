a0847ec9fea22331097a2471d1ebbb758cbd4f41
Vitor Barros <webdev.vitor@gmail.com>
2026-08-06 09:20:49 -0300

    feat(git): add tuicr.nvim integration

---
+++ lua/plugins/tuicr.lua
@@ -0,0 +1,13 @@
+return {
+  "xendarboh/tuicr.nvim",
+  cmd = "Tuicr",
+  keys = {
+    { "<leader>gt", "<cmd>Tuicr<cr>", desc = "Tuicr Review" },
+  },
+  opts = {
+    float = {
+      width = 0.9,
+      height = 0.9,
+    },
+  },
+}
